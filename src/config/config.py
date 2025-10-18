417b44bc66ccd48602b634b4f745bcfcf0982277
XDKAY <sasha.naftaliev.02@mail.ru>
2025-10-18 08:18:11 +0300

    Updated config file

--- src/config/config.py
+++ src/config/config.py
@@ -12,6 +12,7 @@ class TelegramBot:
     token: str
     api_id: str
     api_hash: str
+    session_name: str
 
 
 @dataclass
@@ -24,7 +25,8 @@ def load_config() -> Config:
     telegram_bot = TelegramBot(
         token=os.getenv('TOKEN'),
         api_id=os.getenv('API_ID'),
-        api_hash=os.getenv('API_HASH')
+        api_hash=os.getenv('API_HASH'),
+        session_name=os.getenv('SESSION_NAME')
     )
 
     return Config(telegram_bot=telegram_bot)
