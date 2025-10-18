d8ff41b9f83a2a871103bef7ebca81d9105f6f82
XDKAY <sasha.naftaliev.02@mail.ru>
2025-10-18 08:04:35 +0300

    Handlers of the start and help commands

--- src/handlers/base_commands/base_commands.py
+++ src/handlers/base_commands/base_commands.py
@@ -4,3 +4,28 @@ from aiogram.filters import CommandStart,Command
 
 
 base_commands_router = Router(name=__name__)
+
+
+@base_commands_router.message(CommandStart())
+async def handler_start_message(message: Message):
+    """Обработчик команды /start
+
+    Args:
+        message (Message): получаемые сообщение
+    
+    Return:
+        None
+    """    
+    ...
+
+
+@base_commands_router.message(Command('help'))
+async def handler_help_message(message: Message):
+    """Обработчик команды /help
+
+    Args:
+        message (Message): получаемое сообщение
+    Return:
+        None
+    """ 
+    ...
